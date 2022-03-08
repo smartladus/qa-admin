7d9f3243b97ba58fe7d34d8e7aea837478e0e662
smartladus <smartladu@outlook.com>
2022-03-08 16:11:28 +0800

    type: 新增文件上传参数类型

--- src/api/sys/model/uploadModel.ts
+++ src/api/sys/model/uploadModel.ts
@@ -1,3 +1,14 @@
+import { UploadFile } from 'ant-design-vue/es/upload/interface';
+
+export type UploadParams = {
+  fileList: UploadFile[];
+  multi?: boolean;
+  mode?: string;
+  onUploadProgress?: (progressEvent: ProgressEvent) => void;
+};
+
+export type UploadApi = (params: UploadParams) => Promise<any>;
+
 export interface UploadApiResult {
   message: string;
   code: number;
