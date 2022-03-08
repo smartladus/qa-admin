fd58bd089a02134ad923dad6122a6d6a40e69bef
smartladus <smartladu@outlook.com>
2022-03-08 16:19:30 +0800

    type: 新增供应商类型Model

---
+++ src/api/quality/model/supplierModel.ts
@@ -0,0 +1,14 @@
+export type SupplierModel = {
+  id: string;
+  abbr: string;
+  compId: string;
+  compNameZh: string;
+  addressZh: string;
+  compNameEn: string;
+  addressEn: string;
+  mainProducts: string[];
+  drThr150d: number;
+  drThr1y: number;
+  drThr2y: number;
+  drThr3y: number;
+};
