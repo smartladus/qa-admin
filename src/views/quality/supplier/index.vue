a0306f255d154dfdc838296ec58e1f776b3cdb1c
smartladus <smartladu@outlook.com>
2022-03-08 16:22:54 +0800

    feat: 新增供应商管理页面

---
+++ src/views/quality/supplier/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p-4">
+    <a-button @click="fetchInfo">dd</a-button>
+    {{ info }}
+  </div>
+</template>
+
+<script setup lang="ts" name="suppliers">
+  import { ref } from 'vue';
+  import { getAllSuppliers } from '/@/api/quality/supplier';
+
+  const info = ref();
+
+  const fetchInfo = async () => {
+    info.value = await getAllSuppliers();
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/quality/supplier/supplierTableConfig.ts
@@ -0,0 +1,6 @@
+import { BasicColumn, BasicTableProps } from '/@/components/Table';
+import { UploadSetting } from '/@/views/components/UploadModal/uploadModal';
+import { SupplierModel } from '/@/api/quality/model/supplierModel';
+
+import { StdTableConfig } from '/@/views/components/StdTable';
+
