f80c8c4aad31dde37578e5736ada13413c69d2f1
smartladus <smartladu@outlook.com>
2022-03-14 13:15:48 +0800

    wip: 增加task相关api

--- src/api/cert/task.ts
+++ src/api/cert/task.ts
@@ -1,17 +1,41 @@
 import { defHttp } from '/@/utils/http/axios';
 import { uploadFile } from '/@/api/sys/upload';
 import { UploadApi, UploadParams } from '/@/api/sys/model/uploadModel';
+import { TaskModel, TaskStatModel } from '/@/api/cert/model/taskModel';
 
 const urlPrefix = '/api/cert';
 
 enum Api {
   TASKS = '/tasks',
+  STATS = '/tasks/stats',
 }
 
-export const getAllCertTasks = () => defHttp.get({ url: Api.TASKS }, { urlPrefix });
+export const getAllCertTasks = (): Promise<TaskModel[]> =>
+  defHttp.get({ url: Api.TASKS }, { urlPrefix });
 
-export const getCertTaskByTaskNo = (taskNo: string) =>
+export const getCertTaskByTaskNo = (taskNo: string): Promise<TaskModel> =>
   defHttp.get({ url: `${Api.TASKS}/${taskNo}` }, { urlPrefix });
+
+export const deleteCertTaskByTaskNo = (taskNo: string): Promise<number> =>
+  defHttp.delete({ url: `${Api.TASKS}/${taskNo}` }, { urlPrefix });
+
+export const insertCertTask = (task: TaskModel): Promise<TaskModel> =>
+  defHttp.post(
+    {
+      url: Api.TASKS,
+      data: task,
+    },
+    { urlPrefix },
+  );
+
+export const updateCertTask = (task: TaskModel): Promise<TaskModel> =>
+  defHttp.put(
+    {
+      url: `${Api.TASKS}/${task.taskNo}`,
+      data: task,
+    },
+    { urlPrefix },
+  );
 
 export const uploadCertTasks: UploadApi = (params: UploadParams) =>
   uploadFile(
@@ -28,3 +52,6 @@ export const uploadCertTasks: UploadApi = (params: UploadParams) =>
       urlPrefix: '/api/upload',
     },
   );
+
+export const getAllCertTasksStats = (): Promise<TaskStatModel[]> =>
+  defHttp.get({ url: Api.STATS }, { urlPrefix });
