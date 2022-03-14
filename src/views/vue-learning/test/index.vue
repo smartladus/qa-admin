53007f601db74a1b8999098ff407686dd7d46b2e
smartladus <smartladu@outlook.com>
2022-03-14 13:19:46 +0800

    test: 用于测试，非关键修改

--- src/views/vue-learning/test/index.vue
+++ src/views/vue-learning/test/index.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="p-4">
-    <a-card title="测试">
-
-
-    </a-card>
-  </div>
+  <PageWrapper title="抽屉组件使用示例">
+    <a-button type="primary" class="my-4" @click="openDrawer(true)"> 打开详情Drawer </a-button>
+    <BasicDrawer @register="drawerRegister" :isDetail="true" title="Drawer Title">
+      <p class="h-20">Content Message</p>
+      <template #titleToolbar> toolbar </template>
+    </BasicDrawer>
+  </PageWrapper>
 </template>
 
-<script setup lang="ts" name="Test">
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { BasicDrawer, useDrawer } from '/@/components/Drawer';
+  import { PageWrapper } from '/@/components/Page';
 
+  export default defineComponent({
+    components: { PageWrapper, BasicDrawer },
+    setup() {
+      // 抽屉
+      const [drawerRegister, { openDrawer }] = useDrawer();
+      return {
+        drawerRegister,
+        openDrawer,
+      };
+    },
+  });
 </script>
 
 <style scoped></style>
