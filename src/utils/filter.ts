836cd2c7d9759fbca35f7e82ae40eacb2fdc6f25
smartladus <smartladu@outlook.com>
2022-03-18 12:13:27 +0800

    refactor: filter放进util

---
+++ src/utils/filter.ts
@@ -0,0 +1,24 @@
+import dayjs, { Dayjs } from 'dayjs';
+
+export function numberFormat(val: string | number, fractionDigits = 0): string {
+  if (isNaN(Number(val)) || Number(val) === 0) return '-';
+  const num = `${Number(val).toFixed(fractionDigits)}`;
+  // 将整数部分逢三一断
+  const integer = num.split('.')[0].replace(/(\d)(?=(?:\d{3})+$)/g, '$1,');
+  // 取出小数点部分
+  const decimal = num.lastIndexOf('.') === -1 ? '' : num.split('.')[1];
+  return `${integer}.${decimal}`;
+}
+
+export function replaceDivider(srcTxt: string, divider: string, newDivider: string): string {
+  return (srcTxt && srcTxt.replaceAll?.(divider, newDivider)) ?? '-';
+}
+
+export function dateFormat(date: Dayjs, pattern: string): string {
+  const res = dayjs(date).format(pattern);
+  return res === 'Invalid Date' ? '-' : res;
+}
+
+export function percent(val: string | number, fractionDigits = 0) {
+  return !isNaN(Number(val)) ? `${(Number(val) * 100).toFixed(fractionDigits)}%` : 'N/A';
+}
--- src/views/quality/suppliers/index.vue
+++ src/views/quality/suppliers/index.vue
@@ -1,21 +1,132 @@
 <template>
-  <div class="p-4">
-    <a-button @click="fetchInfo">dd</a-button>
-    {{ info }}
-  </div>
+  <PageWrapper>
+    <StdTable
+      @register="register"
+      :table-config="supplierTableConfig"
+      :defaultAction="false"
+      @new="handleEdit"
+    >
+      <template #bodyCell="{ column, record: supplier }">
+        <template v-if="column.key === 'mainProducts'">
+          <div class="typeContainer">
+            <a-tag
+              v-for="type in supplier.mainProducts"
+              :key="type"
+              color="processing"
+              class="certType"
+            >
+              {{ type }}
+            </a-tag>
+          </div>
+        </template>
+        <template v-if="column.key === 'drRequirement'">
+          <a-descriptions size="small" :column="1">
+            <a-descriptions-item label="早期不良率(150天内)">{{ percent(supplier.drThr150d, 1) }}</a-descriptions-item>
+            <a-descriptions-item label="1年内不良率">{{ percent(supplier.drThr1y, 1) }}</a-descriptions-item>
+            <a-descriptions-item label="2年内不良率">{{ percent(supplier.drThr2y, 1) }}</a-descriptions-item>
+            <a-descriptions-item label="3年内不良率">{{ percent(supplier.drThr3y, 1) }}</a-descriptions-item>
+          </a-descriptions>
+        </template>
+        <template v-if="column.key === 'companyInfoZh'">
+          <a-descriptions size="small" :column="1">
+            <a-descriptions-item label="公司名称">
+              <div class="text-start">{{ supplier.compNameZh }}</div>
+            </a-descriptions-item>
+            <a-descriptions-item label="公司地址">
+              <div class="text-start">{{ supplier.addressZh }}</div>
+            </a-descriptions-item>
+          </a-descriptions>
+        </template>
+        <template v-if="column.key === 'companyInfoEn'">
+          <a-descriptions size="small" :column="1">
+            <a-descriptions-item label="公司名称">
+              <div class="text-start">{{ supplier.compNameEn }}</div>
+            </a-descriptions-item>
+            <a-descriptions-item label="公司地址">
+              <div class="text-start">{{ supplier.addressEn }}</div>
+            </a-descriptions-item>
+          </a-descriptions>
+        </template>
+      </template>
+
+      <template #action="{ record, column }">
+        <TableAction :actions="createActions(record, column)" />
+      </template>
+    </StdTable>
+  </PageWrapper>
 </template>
 
-<script setup lang="ts" name="suppliers">
-  import { ref } from 'vue';
-  import { getAllSuppliers } from '/@/api/quality/supplier';
+<script setup lang="ts">
+  import { getCurrentInstance, provide, ref } from 'vue';
+  import { PageWrapper } from '/@/components/Page';
+  import { StdTable } from '/@/views/components/StdTable';
+  import supplierTableConfig from '/@/views/quality/suppliers/supplierTableConfig';
+  import {
+    TableAction,
+    ActionItem,
+    BasicColumn,
+    EditRecordRow,
+    useTable,
+  } from '/@/components/Table';
+  import { SupplierModel } from '/@/api/quality/model/supplierModel';
+  import { percent } from '/@/utils/filter';
 
-  const info = ref();
+  const [register, { reload }] = useTable({});
 
-  const fetchInfo = async () => {
-    info.value = await getAllSuppliers();
+  function createActions(record: EditRecordRow, column: BasicColumn): ActionItem[] {
+    return [
+      {
+        label: '编辑',
+        icon: 'ant-design:form',
+        onClick: handleEdit.bind(null, record),
+      },
+      {
+        label: '删除',
+        icon: 'ant-design:delete',
+        color: 'error',
+        popConfirm: {
+          title: '是否确认删除',
+          confirm: handleDelete.bind(null, record),
+        },
+      },
+    ];
   }
+
+  const handleEdit = (supplier: Recordable<SupplierModel>) => {
+    console.log(`editing ${supplier.abbr}`);
+    // openDrawer(true);
+    // editingTaskNo.value = (task?.taskNo as unknown as string) ?? 'new';
+  };
+
+  const handleDelete = async (supplier: Recordable<SupplierModel>) => {
+    console.log(`deleting ${supplier.abbr}`);
+    // let count = await deleteCertTaskByTaskNo(task.taskNo);
+    // notification.success({
+    //   message: `成功删除 ${count} 条记录`,
+    //   description: `${task.taskNo} 已删除`,
+    // });
+    // await reload();
+  };
 </script>
 
 <style scoped>
-
+  .typeContainer {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+  .typeContainer .certType {
+    min-width: 48px;
+    display: block;
+    margin: 4px;
+  }
+  .text-start {
+    text-align: start;
+    white-space: normal;
+    word-break: break-all;
+    word-wrap: break-word;
+  }
+  /deep/ .ant-descriptions-row > td {
+    padding-bottom: 0;
+  }
 </style>
